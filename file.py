7a14810eb8b0c15b8c6056f8dea2d4519a8a385a
Logan Day <loganday@Logans-MacBook-Pro.local>
2023-02-12 19:34:03 -0800

    working database

--- file.py
+++
@@ -1,8 +0,0 @@
-import chess.pgn 
-pgn = open("lichess_db_standard_rated_2013-01.pgn", encoding="utf-8")
-first_game = chess.pgn.read_game(pgn)
-print(first_game.headers["Event"])
-board = first_game.board()
-# for move in first_game.mainline_moves():
-#     board.push(move)
-print(first_game.game().next())  
--- my_chess.py
+++ my_chess.py
@@ -12,31 +12,41 @@ while True:
         break
 
 duda_rapport = games[0]
-board = duda_rapport.board()
+board = chess.Board()
 
 
+i = 1
+while True:
+	if i % 2:
+		current_move = input('what will your move be laddie?  ')
+	else:
+		current_move = input("What is your opponent's response?  ")	
+	i += 1
+	board_move = chess.Move.from_uci(current_move)
+	board.push(board_move)
+	print(board)
+	#Removing games with a first move different from the 
+	trimmed_games = []
+	for game in games:
 
-first_move = input('what will your first move be laddie?')
+		if str(game.next().move) == current_move:
+			trimmed_games.append(game.next())
 
-#Removing games with a first move different from the 
-trimmed_games = []
-for game in games:
-	if str(game.next().move) == first_move:
-		trimmed_games.append(game)
-
-
-moves = []
-for game in trimmed_games:
-	moves.append(str(game.next().next().move))
+	if len(trimmed_games) == 0:
+		print("cum")
+		break
+	moves = []
+	for game in trimmed_games:
+		moves.append(str(game.next().move))
 
 
-# Count the number of occurrences of each move
-move_counts = Counter(moves)
+	# Count the number of occurrences of each move
+	move_counts = Counter(moves)
 
-# Get the five most common moves
-top_five = move_counts.most_common(5)
+	# Get the five most common moves
+	top_five = move_counts.most_common(5)
 
-# Print the five most common moves
-for move, count in top_five:
-    print(f"{move}: {count}")
-games = trimmed_games
+	# Print the five most common moves
+	for move, count in top_five:
+		print(f"{move}: {count}")
+	games = trimmed_games
